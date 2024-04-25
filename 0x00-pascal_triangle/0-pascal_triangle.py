a80c70edebbd64c5e75f3b4d107b0b4fe958aee0
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-04-25 18:10:35 +0200

    Add documentation to 0-pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,9 @@ Code to construct pascal's triangle
 
 
 def fact(n):
+    """
+    factorial function
+    """
     a = 1
     for i in range(1, n + 1):
         a *= i
@@ -12,10 +15,15 @@ def fact(n):
 
 
 def combination(n, r):
+    """
+    comb function
+    """
     return fact(n) // (fact(n - r) * fact(r))
 
 
 def pascal_triangle(n):
+      """Create a function def pascal_triangle(n): that returns a list of lists
+    """
     triangle = []
     if n <= 0:
         return triangle
