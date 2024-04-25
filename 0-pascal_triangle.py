76462de92244c16e6d8ecaf4e9ca445dae3b5a73
Youssef <youssefkaramedu@gmail.com>
2024-04-25 09:00:33 -0700

    Add file 0

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def fact(n):
+    a = 1
+    for i in range(1, n + 1):
+        a *= i
+    return a
+
+
+def combination(n, r):
+    return fact(n) // (fact(n - r) * fact(r))
+
+
+def pascal_triangle(n):
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(combination(i, j))
+        triangle.append(row)
+    return triangle
