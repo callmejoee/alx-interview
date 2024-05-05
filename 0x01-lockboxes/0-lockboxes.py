fcd8ead9defbf017ab0c36b0afe448b3011580b7
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-05-05 17:07:27 +0300

    Update for edge cases.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,6 +6,10 @@ def canUnlockAll(boxes):
     """
     Determine if all boxes can be opened.
     """
+
+    if not boxes or not boxes[0]:
+        return False
+        
     n = len(boxes)
     visited = [False] * n
     visited[0] = True
@@ -14,7 +18,7 @@ def canUnlockAll(boxes):
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
-            if not visited[key]:
+            if not visited[key] and key < n:
                 visited[key] = True
                 stack.append(key)
 
